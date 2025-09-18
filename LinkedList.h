1f2978cbeda4b7572264d0904bfbdc1896e18afd
EricAlfonso <ealfonso0793@sdsu.edu>
2025-09-18 16:37:43 -0700

    wonderhoy (fork commit)

--- LinkedList.h
+++ LinkedList.h
@@ -5,6 +5,8 @@
 #include <functional>
 
 // Node class template
+
+//where abstraction is happening
 template<typename T>
 class Node {
 private:
@@ -14,9 +16,11 @@ private:
     // Grant LinkedList access to private members
     template<typename> friend class LinkedList;
 
-public:
+public: //constructor, take the value coming in, assign it to data, make sure next pointer is
+    //null pointer
     Node(T val) : data(val), next(nullptr) {}
 };
+
 
 template<typename T>
 class LinkedList {
@@ -27,7 +31,20 @@ public:
     LinkedList() : head(nullptr) {}
 
     void append(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        if (!head) { //checks if head is nullptr
+            head = newNode;
+        }
+        else {
+            Node<T>* current = head;
+            //moves through linked list until curr is the final node in the list
+            while (current->next) { //checks if current is pointing to a null ptr so it is last node
+                current = current->next; //assigns current to the next node in the linked list
+            }
 
+            //makes current pointer point to newNode
+            current->next = newNode;
+        }
     }
 
     void addFirst(T value) {
@@ -39,6 +56,21 @@ public:
     }
 
     void deleteLast() {
+        if (!head) {
+            return;
+        }
+        if (!head->next) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+
+        Node<T>* current = head;
+        while (current->next->next) {
+            current = current->next;
+        }
+        delete current->next;
+        current->next = nullptr;
 
     }
     void deleteAtIndex(int index) {
@@ -54,7 +86,12 @@ public:
         }
     }
 
-    ~LinkedList() {
+    ~LinkedList() { //destructor
+        while (head) {
+            Node<T>* current = head;
+            head = head->next;
+            delete current;
+        }
 
     }
 };
